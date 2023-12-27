3f27d6a899c614c12757bc09f53c5336293d056b
hideonbush3 <ppoii0961@naver.com>
2023-12-27 18:40:26 +0900

    front service

---
+++ frontend/happypet-react-app/src/service/ApiService.js
@@ -0,0 +1,60 @@
+import { API_BASE_URL } from "../api-config";
+
+export function call(api, method, request){
+    let headers = new Headers({
+        "Content-Type": "application/json",
+    });
+
+    const accessToken = localStorage.getItem("token");
+    if(accessToken && accessToken!==null){
+        headers.append("Authorization", "Bearer " + accessToken);
+    }
+
+    let options = {
+        headers: headers,
+        url: API_BASE_URL + api,
+        method: method,
+    };
+    if(request) {
+        options.body = JSON.stringify(request);
+    }
+    return fetch(options.url, options).then((res) => {
+        console.log(res);
+        if(res.status === 200) {
+            return res.json();
+        }else if(res.status === 403){
+            window.location.href = "/user/login";
+        }else{
+            return res.json();
+        }
+    })
+    .catch(error => {
+        throw error;
+    })
+}
+
+// 로그인
+export function signin(userDTO){
+    return call("/user/signin", "POST", userDTO)
+        .then((res) => {
+            console.log(res);
+            if(res.object !== null){
+                localStorage.setItem("token", res.object.token);
+                window.location.href = "/"
+            }else{
+                if(res.error === '아이디') alert('해당 아이디와 일치하는 회원은 없습니다.');
+                else alert('비밀번호를 다시 확인하세요.');
+            }
+        });
+}
+
+// 로그아웃
+export function signout(){
+    localStorage.setItem("token", null);
+    window.location.href = "/login";
+}
+
+// 회원가입
+export function signup(userDTO){
+    return call("/user/signup", "POST", userDTO);
+}
---
+++ frontend/happypet-react-app/src/service/LoadFacilityListService.js
@@ -0,0 +1,65 @@
+export default function LoadFacilityListService(props){
+    const key = process.env.REACT_APP_API_KEY;
+    fetch("http://localhost:8080/facilityAPI?key=" + key, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          props.setFacilityList(res.data);
+  
+          const container = document.getElementById('kakao-map');
+          const options = {
+            center: new window.kakao.maps.LatLng(37.4198715, 127.126405),
+            level: 7
+          };
+          const map = new window.kakao.maps.Map(container, options);
+          props.setMyMap(map);
+      
+          let markers = [];
+          (res.data).forEach((facility) => {
+            const markerPosition = new window.kakao.maps.LatLng(facility.lat, facility.lot);
+            const marker = new window.kakao.maps.Marker({
+              position: markerPosition,
+            })
+            marker.setMap(map);
+  
+            const content = `<div style="padding:8px; display:flex; align-items:center; justify-content:center; min-width: 150px;max-width: 150px;">
+                                ${facility.name}
+                            </div>`;
+
+            const infowindow = new window.kakao.maps.InfoWindow({
+              content: content
+            });
+            
+            window.kakao.maps.event.addListener(marker, 'mouseover', function() {
+              props.setSelectedFacility(facility);
+              infowindow.open(map, marker);
+            });
+            
+            window.kakao.maps.event.addListener(marker, 'mouseout', function() {
+              infowindow.close();
+            });
+  
+            window.kakao.maps.event.addListener(marker, 'click', function() {
+              props.viewModal(facility);
+              map.setCenter(new window.kakao.maps.LatLng(facility.lat, facility.lot));
+            });
+            markers.push(marker);
+          });
+          props.setMarkerList(markers);
+  
+          let siguns = res.data.map((facility) => facility.sigun);
+          let uniqueSiguns = [...new Set(siguns)];
+          props.setSiguns(uniqueSiguns);
+          
+          let types = res.data.map((facility) => facility.type);
+          let uniqueTypes = [...new Set(types)];
+          props.setTypes(uniqueTypes);
+        })
+        .catch((error) => {
+          console.error("에러 발생: ", error);
+        });
+}
