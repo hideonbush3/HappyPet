7008bfe8dbb6aa1c2b664749a4b5641298deb6aa
hideonbush3 <ppoii0961@naver.com>
2023-12-27 19:02:38 +0900

    index.js

---
+++ frontend/happypet-react-app/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import AppRouter from './AppRouter';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const container = document.getElementById("root");
+
+const root = createRoot(container);
+
+root.render(<AppRouter tab="home" />);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
