e66b9d85b95c3f6b764dec70b10d627d7ae89060
hideonbush3 <ppoii0961@naver.com>
2024-01-06 21:05:27 +0900

    게시글 작성 페이지 이미지 첨부

--- frontend/happypet-react-app/src/pages/Board/Writing.js
+++ frontend/happypet-react-app/src/pages/Board/Writing.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import style from './Writing.module.css';
-import { call } from '../../service/ApiService';
 import { useNavigate } from 'react-router-dom';
+import { API_BASE_URL } from "../../api-config";
 export default function Writing(){
     const navigate = useNavigate();
     const [title, setTitle] = useState('');
@@ -18,16 +18,54 @@ export default function Writing(){
     const handleContentChange = (e) => {
       setContent(e.target.value);
     };
+
   
     const handleSubmit = () => {
-      call('/post', 'POST', {title: title, content: content})
-      .then((res) => {
-        if(res.error !== undefined){
-            alert("게시글 작성을 실패했습니다");
-        }else{
-            navigate('/board/view', {state: {post: res}});
+        const formData = new FormData();
+        formData.append('title', title);
+        formData.append('content', content);
+
+        const imageInput = document.getElementById('imageInput');
+        for (const file of imageInput.files) {
+            formData.append('images', file);
         }
-      })
+
+        const accessToken = localStorage.getItem("token");
+        const options = {
+            url: API_BASE_URL + '/post',
+            method: 'POST',
+            body: formData,
+        };
+        if(accessToken && accessToken!==null){
+            options.headers = {
+                "Authorization": "Bearer " + accessToken,
+            };
+        }
+
+        return fetch(options.url, options).then((res) => {
+            console.log(res);
+            if(res.status === 200) {
+                return res.json();
+            }else if(res.status === 403){
+                window.location.href = "/user/login";
+            }else{
+                return res.json();
+            }
+        })
+        .then((res) => {
+            if(res.error === undefined){
+                navigate('/board/view', {state: {post: res}});
+            }else if(res.error === '존재하지 않는 유저'){
+                alert('세션이 만료됐습니다. 재로그인하세요.');
+                localStorage.setItem('token', null);
+                window.location.href = '/user/login';
+            }else{
+                alert('이미지 파일만 첨부할 수 있습니다.')
+            }
+          })
+        .catch(error => {
+            throw error;
+        })
     };
   
     return(
@@ -53,6 +91,7 @@ export default function Writing(){
                     <button onClick={cancelEventHandler}>취소</button>
                     <button onClick={handleSubmit}>작성 완료</button>               
             </div>
+            <input multiple="multiple" type='file' id='imageInput'/>
 
         </div>
         </div>
