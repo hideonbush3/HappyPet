fed77f5cd7038e31b5f9e65d4259f217dbbe134d
hideonbush3 <ppoii0961@naver.com>
2024-02-16 19:02:59 +0900

    .gitignore 추가

---
+++ src/test/java/hideonbush3/springboot/happypet/HappypetApplicationTests.java
@@ -0,0 +1,13 @@
+package hideonbush3.springboot.happypet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappypetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
