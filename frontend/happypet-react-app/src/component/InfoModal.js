564b6ebc7cd5448e28e8b2349a29dcb7b3c4a11a
hideonbush3 <ppoii0961@naver.com>
2024-03-21 16:55:44 +0900

    즐겨찾기 시 토스트메시지 수정

--- frontend/happypet-react-app/src/component/InfoModal.js
+++ frontend/happypet-react-app/src/component/InfoModal.js
@@ -4,11 +4,15 @@ import { BsGeoAlt, BsHouse, BsTelephone } from "react-icons/bs";
 import { AiOutlineStar, AiFillStar } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
 import { call } from "../service/ApiService";
+import { useState } from "react";
+import ToastMessage from "./ToastMessage";
 
 function InfoModal(props){
     const {facility} = props;
     const {addedToFavorites} = props;
     const navigate = useNavigate();
+    const [showToastMessage, setShowToastMessage] = useState(false);
+    const [process, setProcess] = useState("");
 
     if (!facility || !facility.name) {
       return null;
@@ -18,11 +22,20 @@ function InfoModal(props){
       navigate("/facility/detail", { state: {facility: facility}});
     }
 
+    const callToastMessage = () => {
+      setShowToastMessage(true);
+      setTimeout(() => {
+          setShowToastMessage(false);
+      }, 500)
+  }
+
     const clickAddFavorite = () => {
       call('/favorite', 'POST', facility)
       .then((res) => {
         if(res.error === undefined){
           props.setAddedToFavorites(res);
+          setProcess("즐겨찾기 추가")
+          callToastMessage();
         }else{
           alert("이미 즐겨찾기 추가한 시설입니다.")
         }
@@ -34,6 +47,8 @@ function InfoModal(props){
       .catch(error => {
         console.log(`설계상 에러\n메세지 => `, error);
         props.setAddedToFavorites(null);
+        setProcess("즐겨찾기 삭제")
+        callToastMessage();
       })
     }
     return(
@@ -79,6 +94,8 @@ function InfoModal(props){
                   <button onClick={detailFacilityBtnEventHandler}>상세보기</button>
                 </div>
           </div>
+          <ToastMessage show={showToastMessage} onHide={() => setShowToastMessage(false)} process={process}/>
+
         </Container>
       </Modal.Body>
     </Modal>
