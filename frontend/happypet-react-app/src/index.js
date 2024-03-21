97b8af595a2d825378d47f84b96e2f84335b7e87
hideonbush3 <ppoii0961@naver.com>
2024-03-21 16:50:53 +0900

    필수

--- frontend/happypet-react-app/src/index.js
+++ frontend/happypet-react-app/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
 import AppRouter from './AppRouter';
 import 'bootstrap/dist/css/bootstrap.min.css';
