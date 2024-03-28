c7bc8442c21601b731cda0049bd799a5727304fe
hideonbush3 <ppoii0961@naver.com>
2024-03-28 23:37:31 +0900

    백엔드 이메일 인증 entity

---
+++ src/main/java/hideonbush3/springboot/happypet/model/EmailAuthEntity.java
@@ -0,0 +1,33 @@
+package hideonbush3.springboot.happypet.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "email_auth")
+public class EmailAuthEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    @Column(name = "auth_code")
+    private String authCode;
+    
+    @Column(name = "created_date")
+    private LocalDateTime createdDate;
+}
