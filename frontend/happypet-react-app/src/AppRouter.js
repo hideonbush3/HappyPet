2ecade4883c2643448889ab95731d2a48ca965b8
hideonbush3 <ppoii0961@naver.com>
2024-03-30 15:01:38 +0900

    계정찾기 화면일경우 헤더, 푸터 안보이게

--- frontend/happypet-react-app/src/AppRouter.js
+++ frontend/happypet-react-app/src/AppRouter.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from "react";
-import { BrowserRouter, Route, Routes, useParams } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation, useParams } from "react-router-dom";
 import App from "./App";
 
 import Login from "./pages/User/Login";
@@ -16,32 +16,47 @@ import Writing from './pages/Board/Writing';
 import View from './pages/Board/View';
 import Modify from './pages/Board/Modify';
 import MypageRouter from './pages/User/MyPage/MypageRouter';
+import FindAccount from './pages/User/FindAccount';
+import FindId from './pages/User/FindId';
+import FindPassword from './pages/User/FindPassword';
 
 function ModifyRoute() {
     const { title } = useParams();
     return <Join title={title} />;
   }
+  
+  function AppRouter() {
+      return (
+        <BrowserRouter>
+          <AppContent />
+        </BrowserRouter>
+      );
+    }
+  function AppContent() {
+    const location = useLocation();
 
-function AppRouter(){
-    return(
-        <div>
-            <Header/>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<App />} />
-                    <Route path="/user/login" element={<Login />} />
-                    <Route path="/user/:title" element={<ModifyRoute/>}/>
-                    <Route path="/user/mypage/*" element={<MypageRouter/>}/>
-                    <Route path="/facility/detail" element={<DetailFacility/>} />
-                    <Route path="/board" element={<Board/>} />
-                    <Route path="/board/write" element={<Writing/>}/>
-                    <Route path='/board/view' element={<View/>}/>
-                    <Route path='/board/view/modify' element={<Modify/>}/>
-                </Routes>
-            </BrowserRouter>
-            <Footer />
-        </div>
-    )
+    const shouldShowHeaderAndFooter = !location.pathname.startsWith("/user/findaccount");
+  
+    return (
+      <div>
+        {shouldShowHeaderAndFooter && <Header />}
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/user/login" element={<Login />} />
+          <Route path='/user/findaccount' element={<FindAccount />} />
+          <Route path="/user/findaccount/id" element={<FindId />} />
+          <Route path="/user/findaccount/password" element={<FindPassword />} />
+          <Route path="/user/:title" element={<ModifyRoute />} />
+          <Route path="/user/mypage/*" element={<MypageRouter />} />
+          <Route path="/facility/detail" element={<DetailFacility />} />
+          <Route path="/board" element={<Board />} />
+          <Route path="/board/write" element={<Writing />} />
+          <Route path='/board/view' element={<View />} />
+          <Route path='/board/view/modify' element={<Modify />} />
+        </Routes>
+        {shouldShowHeaderAndFooter && <Footer />}
+      </div>
+    );
 }
 
 export default AppRouter;
