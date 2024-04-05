caefb56850d9208e7f964b22a1d806a9b0f88eb0
hideonbush3 <ppoii0961@naver.com>
2024-04-05 18:15:50 +0900

    사용하지 않는 패키지 삭제

--- src/main/java/hideonbush3/springboot/happypet/controller/FacilityController.java
+++ src/main/java/hideonbush3/springboot/happypet/controller/FacilityController.java
@@ -3,21 +3,11 @@ package hideonbush3.springboot.happypet.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import hideonbush3.springboot.happypet.dto.FacilityDTO;
 import hideonbush3.springboot.happypet.dto.ResponseDTO;
 import hideonbush3.springboot.happypet.service.FacilityService;
-
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
--- src/main/java/hideonbush3/springboot/happypet/controller/ReplyController.java
+++ src/main/java/hideonbush3/springboot/happypet/controller/ReplyController.java
@@ -1,8 +1,5 @@
 package hideonbush3.springboot.happypet.controller;
 
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
